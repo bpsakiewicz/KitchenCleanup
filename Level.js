8c57bf5c91138cfaf625ea0335cdc817846f6598
bpsakiewicz <bpsakiewicz@gmail.com>
2022-04-05 13:04:18 -0600

    Some more basics

--- Level.js
+++ Level.js
@@ -2,12 +2,16 @@ class Level {
 
     constructor(levelNum) {
         this.rooms = this.generateRooms(levelNum);
+        this.enemies = []
+        for(let room in this.rooms) {
+            this.enemies.push(room.enemies);
+        }
     }
 
     generateRooms(levelNum) {
         const rooms = [];
         for(let i = 0; i < levelNum + sqrt(levelNum); i++) {
-            rooms[i] = new Enemy();
+            rooms[i] = new Room(levelNum);
         }
         return rooms;
     }
