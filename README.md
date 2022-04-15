2ebf2ecd86015cba904fec616657d2525211190f
Liam <liamsemeria@gmail.com>
2022-04-15 15:19:56 -0600

    gh-pages link, img test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Kitchen Cleanup
 ## Benny Sakiewicz, Liam Semeria, Samuel Mast
+link to github pages site: https://bpsakiewicz.github.io/KitchenCleanup/
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-
+var LOAD_IMAGES = 1
 var player = new Entity(new p5.Vector(50,400), new p5.Vector(400,0), "player", 40,"box");
 var g = new Game(player,new p5.Vector(1175,775));
 var TESTBUMPER = new Entity(new p5.Vector(320,415), new p5.Vector(0,0),"BOX",100,"box");
@@ -6,7 +6,9 @@ var TESTBUMPER1 = new Entity(new p5.Vector(600,315), new p5.Vector(0,00),"CIRCLE
 var timelastcalled;
 let img;
 function preload() {
-  //img = loadImage("assets/classic_cook.png");
+  if (LOAD_IMAGES) {
+    img = loadImage("assets/classic_cook.png");
+  }
 }
 
 function setup() {
@@ -15,14 +17,14 @@ function setup() {
   timelastcalled = millis();
   g.instantiate(TESTBUMPER);
   g.instantiate(TESTBUMPER1);
-  //player.setImage(img)
+  player.setImage(img)
 }
 
 function draw() {
   background(100,100,130);
   var time = millis();
   var dt = (time - timelastcalled) / 1000;
-  //image(img,0,0)
+  image(img,0,0)
   fill(color(0,0,0))
   g.draw();
   g.update(dt);
