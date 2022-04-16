7462cce31d4d636b5dd24789808153ecad924f05
Liam <liamsemeria@gmail.com>
2022-04-15 20:28:49 -0600

    added bullet sprites

--- Enemy.js
+++ Enemy.js
@@ -1,6 +1,6 @@
 class Enemy extends Entity{
     constructor(enemyType) {
-        super(new p5.Vector(random(100, width - 100), random(100, height - 100)), new p5.Vector(2, 2), "enemy", "box", new p5.Vector(100, 100))
+        super(new p5.Vector(random(100, width - 100), random(100, height - 100)), new p5.Vector(2, 2), "enemy", "circle", new p5.Vector(100, 100))
         this.setImage(tomato)
     }
 
